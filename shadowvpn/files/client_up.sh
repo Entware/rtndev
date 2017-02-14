8420a5ce4d4bf3100236571c71242997361e060e
The-BB <tun.chen.bo@gmail.com>
2017-02-14 21:58:26 +0300

    shadowvpn: Entware-ng sync, 2017.02

---
+++ shadowvpn/files/client_up.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# This script will be executed when client is up. All key value pairs (except
+# password) in ShadowVPN config file will be passed to this script as
+# environment variables.
+
+# Turn on IP forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+# Configure IP address and MTU of VPN interface
+ip addr add $net dev $intf
+ip link set $intf mtu $mtu
+ip link set $intf up
+
+# Turn on NAT over VPN
+iptables -t nat -A POSTROUTING -o $intf -j MASQUERADE
+iptables -I FORWARD 1 -i $intf -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD 1 -o $intf -j ACCEPT
+
+# Direct route to VPN server's public IP via current gateway
+ip route add $server via $(ip route show 0/0 | sort -k 7 | head -n 1 | sed -e 's/.* via \([^ ]*\).*/\1/')
+
+# Shadow default route using two /1 subnets
+ip route add   0/1 dev $intf
+ip route add 128/1 dev $intf
+
+echo $0 done
